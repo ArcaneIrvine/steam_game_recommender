bc4907fd3c5cde6787d2170484aa4b759957ae33
MakisEu <euaggelinosmakis@gmail.com>
2023-06-16 19:45:42 +0300

    Updated ids and id_scanner

--- src/data/ids_scanner.py
+++ src/data/ids_scanner.py
@@ -4,6 +4,7 @@ import csv
 from steam import Steam
 from decouple import config
 from pprint import pprint
+import os
 
 # Get the Steam Web API key
 KEY = config("STEAM_API_KEY")
@@ -13,10 +14,32 @@ steam = Steam(KEY)
 base_url = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/'
 
 random_steam_ids = []
-steam_id = 76561198327129152 # initial steam_id
- 
+#steam_id = 76561198327129152
+starting = 76561198327139135
+steam_id = 76561198327139135# initial steam_id
+dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path=dir_path.replace("/src/data",'')
+
+
+# Start where you left
+try:
+    with open(dir_path+'/data/raw/steam_ids.csv', 'r') as f:
+        last_line = f.readlines()[-1]
+        if (len(last_line)>3):
+            steam_id=int(last_line)
+except FileNotFoundError:
+    print("File not found. Creating from scratch...")
+
+# File to write
+write=open(dir_path+'/data/raw/steam_ids.csv', 'a', newline='')
+writer = csv.writer(write)
+if (starting==steam_id):
+    writer.writerow(['Steam ID'])
+added=0
+
+print("Starting id search...Press ctrl+c to stop")
 # Increment the initial steam_id by 1 for x repetitions
-for _ in range(10000):
+for i in range(10000):
     # Final URL with the correct key and paramaters for the request
     URL = f'{base_url}?key={KEY}&steamids={steam_id}'
     response = requests.get(URL)
@@ -27,7 +50,7 @@ for _ in range(10000):
     # If there is a User with the given ID
     if 'players' in data['response'] and len(data['response']['players']) > 0:
         player = data['response']['players'][0]
-        
+
         # If communityvisibilitystate and profilestate exist in the user's data
         if 'communityvisibilitystate' in player and 'profilestate' in player:
             community_visibility_state = player['communityvisibilitystate']
@@ -36,17 +59,18 @@ for _ in range(10000):
             # If the user has a profile and the profile is public
             if community_visibility_state == 3 and profile_state == 1:
                 user_games = steam.users.get_owned_games(steam_id)
-                
+
                 # If the user has more than 1 games
                 if 'game_count' in user_games:
                     if user_games['game_count'] >= 1:
                         # add the user id in the array
                         pprint(user_games)
-                        random_steam_ids.append(steam_id)
+                        writer.writerow([steam_id])
+                        added+=1
                     else:
                         print("User", steam_id, "has no games")
                 else:
-                    print("User", steam_id, "has no games")
+                     print("User", steam_id, "has no games")
 
             else:
                 print("communityvisibilitystate is not 3 and profile_state is not 1 for user", steam_id)
@@ -58,11 +82,11 @@ for _ in range(10000):
 
     # increment by 1
     steam_id += 1
-    
-print("Total IDs gathered:", len(random_steam_ids))
 
+print("Total IDs gathered:", added)
+write.close()
 # Save the IDs to a CSV file
-with open('data/raw/steam_ids.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Steam_ID'])  # Write the header row
-    writer.writerows([[id] for id in random_steam_ids])
+#with open('data/useable_steam_ids.csv', 'w', newline='') as file:
+#    writer = csv.writer(file)
+ #   writer.writerow(['Steam ID'])  # Write the header row
+  #  writer.writerows([[id] for id in random_steam_ids])
